511b9b321d6df377ed071edc6f1dd24222ab884f
akirion <akiriondev@gmail.com>
2016-03-24 02:01:37 +0100

    Add some more tests.

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -15,4 +15,22 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+    
+    public function testLog()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/log');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+    
+    public function testSettings()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/settings');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 }
