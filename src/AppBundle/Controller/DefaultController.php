a454a9d0cb305dd66fd752e232120fe1b0a2878e
akirion <akiriondev@gmail.com>
2016-04-09 14:26:09 +0200

    Add CoreConnector

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -22,12 +22,7 @@ class DefaultController extends Controller
   {
     return $this->render('AppBundle:Default:settings.html.twig');
   }
-  
-  public function logAction()
-  {
-    return $this->render('AppBundle:Default:log.html.twig');
-  }
-  
+
   public function settingsUsersAction()
   {
     return $this->render('AppBundle:Default:settings_users.html.twig');
@@ -38,9 +33,33 @@ class DefaultController extends Controller
     return $this->render('AppBundle:Default:settings_user_details.html.twig');
   }  
   
+  public function addUserAction()
+  {
+  }
+  
+  public function updateUserAction()
+  {
+  }
+  
+  public function deleteUserAction()
+  {
+  }
+  
   public function settingsSiteMapAction()
   {
     return $this->render('AppBundle:Default:settings_sitemap.html.twig');
+  }
+  
+  public function addSiteMapItemAction()
+  {
+  }
+  
+  public function updateSiteMapItemAction()
+  {
+  }
+  
+  public function deleteSiteMapItemAction()
+  {
   }
   
   public function settingsRulesAction()
@@ -48,10 +67,41 @@ class DefaultController extends Controller
     return $this->render('AppBundle:Default:settings_rules.html.twig');
   }
   
+  public function settingsRuleAction($id)
+  {
+    
+  }
+  
+  public function addRuleAction()
+  {
+  }
+  
+  public function updateRuleAction()
+  {
+  }
+  
+  public function deleteRuleAction()
+  {
+  }
+  
   public function settingsDevicesAction()
   {
     return $this->render('AppBundle:Default:settings_devices.html.twig');
   }
   
-
+  public function settingsDeviceDetailsAction()
+  {
+    
+  }
+  
+  public function logAction()
+  {
+    return $this->render('AppBundle:Default:log.html.twig');
+  }
+  
+  public function logMessagesAction($offset, $count)
+  {
+    return $this->render('AppBundle:Default:log.html.twig');
+  }
+  
 }
